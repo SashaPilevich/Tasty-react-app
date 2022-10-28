655028368f1e97baff234a6901c84f2d7e510d2f
Pilevich Sasha <pilevichsasha@gmail.com>
2022-10-28 21:53:57 +0300

    add local validation for delivery form

--- tasty-react-app/src/components/Delivery/index.tsx
+++ tasty-react-app/src/components/Delivery/index.tsx
@@ -1,9 +1,14 @@
-import { MouseEvent } from "react";
+import { ChangeEventHandler, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../Button";
 import { Input } from "../Input";
 import style from "./style.module.css";
 import { NotificationManager } from "react-notifications";
+import {
+  validateAddress,
+  validatePhoneNumber,
+  validateRequired,
+} from "../../utils/validation";
 
 export const Delivery = () => {
   const navigate = useNavigate();
@@ -12,24 +17,169 @@ export const Delivery = () => {
     navigate("/category");
     localStorage.removeItem("shopList");
   };
+  const [citySt, setCitySt] = useState("");
+  const [houseApartment, setHouseApartment] = useState("");
+  const [entranceFloor, setEntranceFloor] = useState("");
+  const [date, setDate] = useState("");
+  const [telNumber, setTelNumber] = useState("");
+
+  const [cityStError, setCityStError] = useState("");
+  const [houseApartmentError, setHouseApartmentError] = useState("");
+  const [entranceFloorError, setEntranceFloorError] = useState("");
+  const [dateError, setDateError] = useState("");
+  const [telNumberError, setTelNumberError] = useState("");
+
+  //cityStreet
+  const handleCityStreet: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const error = validateRequired(event.target.value);
+    if (error) {
+      setCityStError(error);
+    } else {
+      setCityStError("");
+    }
+    setCitySt(event.target.value);
+  };
+  const handleCityBlur = () => {
+    const error = validateRequired(citySt);
+    setCityStError(error);
+  };
+  const handleCityFocus = () => {
+    setCityStError("");
+  };
+
+  //houseAppart
+  const handleHouseApartment: ChangeEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    const error = validateAddress(event.target.value);
+    if (error) {
+      setHouseApartmentError(error);
+    } else {
+      setHouseApartmentError("");
+    }
+    setHouseApartment(event.target.value);
+  };
+  const handleHouseApartmentBlur = () => {
+    const error = validateAddress(houseApartment);
+    setHouseApartmentError(error);
+  };
+  const handleHouseApartmentFocus = () => {
+    setHouseApartmentError("");
+  };
+
+  //entranceFloor
+  const handleEntranceFloor: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const error = validateAddress(event.target.value);
+    if (error) {
+      setEntranceFloorError(error);
+    } else {
+      setEntranceFloorError("");
+    }
+    setEntranceFloor(event.target.value);
+  };
+  const handleEntranceFloorBlur = () => {
+    const error = validateAddress(entranceFloor);
+    setEntranceFloorError(error);
+  };
+  const handleEntranceFloorFocus = () => {
+    setEntranceFloorError("");
+  };
+
+  //date
+  const handleDate: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const error = validateRequired(event.target.value);
+    if (error) {
+      setDateError(error);
+    } else {
+      setDateError("");
+    }
+    setDate(event.target.value);
+  };
+  const handleDateBlur = () => {
+    const error = validateRequired(date);
+    setDateError(error);
+  };
+  const handleDateFocus = () => {
+    setDateError("");
+  };
+
+  //phoneNumber
+  const handleTelNumber: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const error = validatePhoneNumber(event.target.value);
+    if (error) {
+      setTelNumberError(error);
+    } else {
+      setTelNumberError("");
+    }
+    setTelNumber(event.target.value);
+  };
+  const handleTelNumberBlur = () => {
+    const error = validatePhoneNumber(telNumber);
+    setTelNumberError(error);
+  };
+  const handleTelNumberFocus = () => {
+    setTelNumberError("");
+  };
   return (
     <div className={style.container}>
       <div className={style.address}>
         <h3 className={style.title}>Адрес доставки</h3>
+
         <div className={style.inputContainer}>
-          <Input uniqType="delivery" label="Город/Улица" />
+          <Input
+            uniqType="inputForRegistration"
+            label="Город/Улица"
+            onChange={handleCityStreet}
+            placeholder="город/улица"
+            error={cityStError}
+            onBlur={handleCityBlur}
+            onFocus={handleCityFocus}
+          />
         </div>
         <div className={style.inputContainer}>
-          <Input uniqType="delivery" label="Дом/Квартира" />
+          <Input
+            uniqType="delivery"
+            label="Дом/Квартира"
+            onChange={handleHouseApartment}
+            onFocus={handleHouseApartmentFocus}
+            onBlur={handleHouseApartmentBlur}
+            error={houseApartmentError}
+            placeholder="дом/квартира"
+          />
         </div>
         <div className={style.inputContainer}>
-          <Input uniqType="delivery" label="Подъезд/Этаж" />
+          <Input
+            uniqType="delivery"
+            label="Подъезд/Этаж"
+            onChange={handleEntranceFloor}
+            placeholder="подъезд/этаж"
+            onFocus={handleEntranceFloorFocus}
+            onBlur={handleEntranceFloorBlur}
+            error={entranceFloorError}
+          />
         </div>
         <div className={style.inputContainer}>
-          <Input uniqType="delivery" label="Дата" type="date" />
+          <Input
+            uniqType="delivery"
+            label="Дата"
+            type="date"
+            onChange={handleDate}
+            onFocus={handleDateFocus}
+            onBlur={handleDateBlur}
+            error={dateError}
+          />
         </div>
         <div className={style.inputContainer}>
-          <Input uniqType="delivery" label="Номер телефона" type="tel" />
+          <Input
+            uniqType="delivery"
+            label="Номер телефона"
+            type="tel"
+            onChange={handleTelNumber}
+            onBlur={handleTelNumberBlur}
+            onFocus={handleTelNumberFocus}
+            error={telNumberError}
+            placeholder="+375xx-xxx-xx-xx"
+          />
         </div>
       </div>
       <div className={style.pay}>
--- tasty-react-app/src/utils/validation.ts
+++ tasty-react-app/src/utils/validation.ts
@@ -1,6 +1,8 @@
 const emailRegExp = /(^|\s+)[\w\-.]+@([\w-]+\.)+[\w-]{2,4}($|\s+)/;
 const latinSymbol = /[A-Za-z]/;
 const passwordRegExp = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])/;
+const addressRegExp = /^[ 0-9 /]+$/;
+const telRegExp = /^((7|\+3)[\- ]?)?(\(?\d{3,4}\)?[\- ]?)?[\d\- ]{5,10}$/;
 
 export const validateRequired = (value: string) => {
   if (value === "") {
@@ -35,6 +37,25 @@ export const validateConfirmPassword = (
 ): any => {
   if (password !== confirmPassword) {
     return "Пароли не совпадают";
+  }
+  return "";
+};
+
+export const validateAddress = (value: string) => {
+  if (value === "") {
+    return "Обязательное поле";
+  }
+  if (!addressRegExp.test(value)) {
+    return "Неправильный формат адреса";
+  }
+  return "";
+};
+export const validatePhoneNumber = (value: string) => {
+  if (value === "") {
+    return "Обязательное поле";
+  }
+  if (!telRegExp.test(value)) {
+    return "Неправильный формат номера телефона";
   }
   return "";
 };
